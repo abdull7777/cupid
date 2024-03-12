4ca0bb7c7113f592ca39aab92fc292686a4bf7f4
abdull7777 <aalbshyry02@gmail.com>
2024-03-12 13:05:02 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'modules/splash.dart';
+import 'shared/style/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cupid Arrow',
+      theme: ThemeData(
+        primaryColor: MainColor,
+          scaffoldBackgroundColor: Background,
+          appBarTheme: AppBarTheme(
+            elevation: 0.0,
+            backgroundColor: Background,
+            centerTitle: true,
+
+          )
+      ),
+      home: SplashScreen()
+    );
+  }
+}
